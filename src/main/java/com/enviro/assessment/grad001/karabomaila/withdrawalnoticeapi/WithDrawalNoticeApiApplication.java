8d28bc5bf3948b2b1d41e232c4fcf272c9f87fe0
Karabo_Maila <karabo.maila@wits.ac.za>
2023-08-25 16:07:33 +0200

    new repo

---
+++ src/main/java/com/enviro/assessment/grad001/karabomaila/withdrawalnoticeapi/WithDrawalNoticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.karabomaila.withdrawalnoticeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithDrawalNoticeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithDrawalNoticeApiApplication.class, args);
+    }
+
+}
