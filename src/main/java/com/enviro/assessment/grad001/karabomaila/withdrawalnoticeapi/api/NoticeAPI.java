a299c462e9569d50dc45ab56b2a31ffc7a764c37
Karabo_Maila <karabo.maila@wits.ac.za>
2023-08-25 16:18:03 +0200

    updates

---
+++ src/main/java/com/enviro/assessment/grad001/karabomaila/withdrawalnoticeapi/api/NoticeAPI.java
@@ -0,0 +1,8 @@
+package com.enviro.assessment.grad001.karabomaila.withdrawalnoticeapi.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticeAPI {
+
+}
